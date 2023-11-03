74f75931e7bd42197f12f338e197f5b40c3c5d73
mbwood92 <malikabythewood4@gmail.com>
2023-11-03 01:51:10 -0400

    changes

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-
 const mycardsEl = document.querySelectorAll('.card')
-let checkFlip = false;
+let isFlip = false;
 let firstCard, secondCard;
-let lockCard = false;
+let cardLock = false;
 
 function flipCard() {
-    if (lockCard) return;
+    if (cardLock) return;
     if (this === firstCard) return;
     this.classList.add('flip');
-    if (!checkFlip){
-      checkFlip = true;
+    if (!isFlip){
+        isFlip = true;
       firstCard = this;
       return;
     }
     secondCard = this;
-    checkFlip = false;
-  }
-/*
-  if(firstCard===secondCard)
-  lockcard=true;
+    isFlip = false;
 
-  firstCard.classList.remove('flip')
-  secondCard.classList.remove('flip')
-  lockcard=false;
-  */
+    goodMatch();
+  }
+  function goodMatch() {
+    if (firstCard.dataset.food === secondCard.dataset.food) {
+      disableCards();
+      return;
+    }
+ 
+    unflipCards();
+  }
+ 
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+  }
+ 
+  function unflipCards() {
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+    }, 1000);
+  }
 
 mycardsEl.forEach(card => card.addEventListener('click', flipCard));
-
