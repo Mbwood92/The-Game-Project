4c4c7a76e1ed0ecc5d41ef9380082684aeb1f86c
mbwood92 <malikabythewood4@gmail.com>
2023-11-12 23:15:14 -0500

    changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const mycardsEl = document.querySelectorAll('.card')
+const mycardsEl = document.querySelectorAll('.card');
 let isFlip = false;
 let firstCard, secondCard;
 let cardLock = false;
+
 
 function flipCard() {
     if (cardLock) return;
@@ -17,14 +18,14 @@ function flipCard() {
 
     goodMatch();
 }
+
 function goodMatch() {
-    let isMatch = firstCard.dataset.food === secondCard.dataset.food
+    let isMatch = firstCard.dataset.food === secondCard.dataset.food;
     if (isMatch)
         disableCards();
     else {
         unflipCards();
     }
-
 }
 
 function disableCards() {
@@ -33,10 +34,20 @@ function disableCards() {
 }
 
 function unflipCards() {
+    cardLock = true; // Prevent flipping other cards during the delay
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        cardLock = false; // Allow flipping cards again
     }, 1500);
 }
 
+function resetGame() {
+    mycardsEl.forEach(card => {
+        card.classList.remove('flip');
+        card.addEventListener('click', flipCard);
+    });
+}
+
 mycardsEl.forEach(card => card.addEventListener('click', flipCard));
+document.getElementById('resetButton').addEventListener('click', resetGame);
